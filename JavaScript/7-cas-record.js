2c46030a58282d17e6545732f7ce6458a9138b8a
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-07-24 08:44:26 +0300

    Improve record examples

--- JavaScript/7-cas-record.js
+++ JavaScript/7-cas-record.js
@@ -1,13 +1,8 @@
 'use strict';
 
 class CASRecord {
-  #value;
+  #value = null;
   #hash;
-
-  constructor() {
-    this.#value = null;
-    this.#hash = '';
-  }
 
   static async hash(value) {
     const data = new TextEncoder().encode(JSON.stringify(value));
@@ -24,7 +19,7 @@ class CASRecord {
     };
   }
 
-  async cas(expectedHash, value) {
+  async cas(value, expectedHash) {
     if (this.#hash !== expectedHash) return false;
     this.#value = value;
     this.#hash = await CASRecord.hash(value);
@@ -38,24 +33,24 @@ const main = async () => {
   const reg = new CASRecord();
 
   const record1 = { value: 13.5, unit: 'm/s', precision: 0.1 };
-  await reg.cas('', record1);
+  await reg.cas(record1);
   const state1 = reg.read();
   console.log('Initial:', state1);
 
   const record2 = { value: 13.6, unit: 'm/s', precision: 0.01 };
-  const res2 = await reg.cas(state1.hash, record2);
+  const res2 = await reg.cas(record2, state1.hash);
   const state2 = reg.read();
   console.log('CAS success:', res2);
   console.log('State:', state2);
 
   const record3 = { value: 13.2, unit: 'm/s', precision: 0.001 };
-  const res3 = await reg.cas(state1.hash, record3);
+  const res3 = await reg.cas(record3, state1.hash);
   const state3 = reg.read();
   console.log('CAS success:', res3);
   console.log('State:', state3);
 
   const record4 = { value: 13.4, unit: 'm/s', precision: 0.1 };
-  const res4 = await reg.cas(state2.hash, record4);
+  const res4 = await reg.cas(record4, state2.hash);
   const state4 = reg.read();
   console.log('CAS success:', res4);
   console.log('State:', state4);
